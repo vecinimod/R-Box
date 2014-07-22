a1c839e00d616f3c2ea538efc2ba68ad0b1c3a87
Randy Lai <randy.cs.lai@gmail.com>
2014-07-22 12:35:27 -0700

    st2 update

--- completions.py
+++ completions.py
@@ -2,16 +2,19 @@ import sublime
 import sublime_plugin
 import os
 import json
+import re
 from itertools import chain
-from misc import *
-import codecs
+from .misc import *
 
 def load_jsonfile():
     jsonFilepath = os.path.join(sublime.packages_path(), 'R-Box', 'completions.json')
-    jsonFile = codecs.open(jsonFilepath, "r", encoding="utf-8")
+    jsonFile = open(jsonFilepath, "r", encoding="utf-8")
     data = json.load(jsonFile)
     jsonFile.close()
     return data
+
+def valid(str):
+    return re.match('^[\w._]+$', str) is not None
 
 class RBoxCompletions(sublime_plugin.EventListener):
     completions = None
@@ -23,5 +26,9 @@ class RBoxCompletions(sublime_plugin.EventListener):
         if not self.completions:
             j = dict(load_jsonfile())
             self.completions = list(chain.from_iterable(j.values()))
-            self.completions = [(p, p) for p in self.completions if type(p) == unicode ]
-        return (self.completions,  sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+            self.completions = [(p, ) for p in self.completions if type(p) == str ]
+
+        default_completions = [(item, ) for item in view.extract_completions(prefix) if len(item) > 3 and valid(item)]
+
+        r = list(set(self.completions+default_completions))
+        return (r,  0)
